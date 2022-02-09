0b0a3b742fd8b79042c0698f9ed1d611062a4a43
Reema Chattopadhyay <95260889+notifyreema@users.noreply.github.com>
2022-02-10 09:55:52 +1100

    Updated product.js

---
+++ product.js
@@ -0,0 +1,27 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+    123: {
+      name : "Smart Cam",
+      desc : "Best way to secure your home",
+      img : "dummy-pdt-c.jpg",
+      price : 599
+    },
+    124: {
+      name : "Digi Watch",
+      desc : "All the things of a smartphone in your wrist",
+      img : "dummy-pdt-b.jpg",
+      price : 1199
+    },
+    125: {
+      name : "Smartview",
+      desc : "Seeing is not always believing but hearing and experiencing too",
+      img : "dummy-pdt-a.jpg",
+      price : 1299
+    },
+    126: {
+      name : "Digi Recorder",
+      desc : "When recording notes is just at the click of a button",
+      img : "dummy-pdt-d.jpg",
+      price : 799
+    }
+  };
